78f88ab406ae1bea2369e970587aa5875deb3904
Drew <drewx2@gmail.com>
2013-02-02 17:28:19 -0800

    misc changes

--- system/device/htc/msm8960-common/BoardConfigCommon.mk
+++ system/device/htc/msm8960-common/BoardConfigCommon.mk
@@ -25,54 +25,51 @@
 # against the traditional rules of inheritance).
 
 # Include path
-TARGET_SPECIFIC_HEADER_PATH := device/htc/msm8960-common/include
+TARGET_SPECIFIC_HEADER_PATH 		:= device/htc/msm8960-common/include
 
-BOARD_VENDOR := htc
+BOARD_VENDOR 				:= htc
 
 # Bootloader
-TARGET_NO_BOOTLOADER := true
+TARGET_NO_BOOTLOADER 			:= true
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/htc/msm8960
+TARGET_KERNEL_SOURCE 			:= kernel/htc/msm8960
 
 # Platform
-TARGET_BOARD_PLATFORM := msm8960
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno320
-TARGET_BOOTLOADER_BOARD_NAME := MONARUDO
-TARGET_BOOTLOADER_NAME=monarudo
+TARGET_BOARD_PLATFORM 			:= msm8960
+TARGET_BOARD_PLATFORM_GPU 		:= qcom-adreno320
+TARGET_BOOTLOADER_BOARD_NAME 		:= MONARUDO
+TARGET_BOOTLOADER_NAME 			:= monarudo
 
 # Architecture
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
-
-#TARGET_CPU_ABI := armeabi-v7a
-#TARGET_CPU_ABI2 := armeabi
-#TARGET_ARCH_VARIANT := armv7-a-neon
-#TARGET_ARCH_VARIANT_CPU := cortex-a9
-#TARGET_CPU_SMP := true
-#TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_GLOBAL_CFLAGS 			+= -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS 			+= -mfpu=neon -mfloat-abi=softfp
+TARGET_CPU_ABI 				:= armeabi-v7a
+TARGET_CPU_ABI2 			:= armeabi
+TARGET_CPU_SMP 				:= true
+TARGET_ARCH 				:= arm
+TARGET_ARCH_VARIANT 			:= armv7-a-neon
+TARGET_ARCH_VARIANT_CPU			:= cortex-a9
+ARCH_ARM_HAVE_TLS_REGISTER 		:= true
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION 	:= true
 
 # Flags
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS 			+= -DQCOM_HARDWARE
 
 # Preload bootanimation
-TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_PRELOAD 		:= true
 
 # QCOM hardware
-BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_HARDWARE 		:= true
 
 # Audio   added in BoardConfig.mk
-BOARD_USES_ALSA_AUDIO := true
+BOARD_USES_ALSA_AUDIO 			:= true
+BOARD_USES_FLUENCE_INCALL 		:= true
+BOARD_USES_SEPERATED_AUDIO_INPUT 	:= true
 
 # Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-TARGET_CUSTOM_BLUEDROID := ../../../device/htc/msm8960-common/bluetooth/bluetooth.c
+BOARD_HAVE_BLUETOOTH 			:= true
+TARGET_CUSTOM_BLUEDROID 		:= ../../../device/htc/msm8960-common/bluetooth/bluetooth.c
 
 # FM radio
 #BOARD_HAVE_QCOM_FM := true
@@ -90,16 +87,21 @@ BOARD_EGL_CFG := device/htc/msm8960-common/configs/egl.cfg
 BOARD_EGL_NEEDS_LEGACY_FB := true
 USE_OPENGL_RENDERER := true
 TARGET_USES_ION := true
-TARGET_USES_OVERLAY := true
-TARGET_USES_SF_BYPASS := true
-
+#TARGET_USES_OVERLAY := true
 
 
 # Wifi
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_WLAN_DEVICE                := qcwcn
+BOARD_HAS_QCOM_WLAN := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_MODULE_NAME := prima_wlan
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/prima_wlan.ko"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Webkit
 ENABLE_WEBGL := true 
